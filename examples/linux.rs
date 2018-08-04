5795879b99d21416cb7e33f432c1d7f8a80ab4e0
Diego Barrios Romero <eldruin@gmail.com>
2018-08-04 14:11:06 +0200

    Add example

---
+++ examples/linux.rs
@@ -0,0 +1,23 @@
+extern crate embedded_hal;
+extern crate linux_embedded_hal;
+extern crate at24cxxx;
+
+use embedded_hal::blocking::delay::DelayMs;
+use linux_embedded_hal::{I2cdev, Delay};
+use at24cxxx::{At24cxxx, SlaveAddr};
+
+fn main() {
+    let dev = I2cdev::new("/dev/i2c-1").unwrap();
+    let mut eeprom = At24cxxx::new(dev, SlaveAddr::default().addr());
+    let memory_address = [0x12, 0x34];
+    let data = 0xAB;
+
+    eeprom.write_byte(&memory_address, data).unwrap();
+
+    Delay.delay_ms(5u16);
+
+    let retrieved_data = eeprom.read_byte(&memory_address).unwrap();
+
+    println!("Read memory address: [{},{}], retrieved content: {}",
+             memory_address[0], memory_address[1], &retrieved_data);
+}
