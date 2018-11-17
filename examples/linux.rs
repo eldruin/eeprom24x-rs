34f2d7cbd767d2d26fcb2841d2dc1dc07683fd8c
Diego Barrios Romero <eldruin@gmail.com>
2018-11-17 14:56:44 +0100

    Improve example

--- examples/linux.rs
+++ examples/linux.rs
@@ -3,8 +3,8 @@ extern crate linux_embedded_hal;
 extern crate eeprom24x;
 
 use embedded_hal::blocking::delay::DelayMs;
-use linux_embedded_hal::{I2cdev, Delay};
-use eeprom24x::{Eeprom24x, SlaveAddr};
+use linux_embedded_hal::{ I2cdev, Delay };
+use eeprom24x::{ Eeprom24x, SlaveAddr };
 
 fn main() {
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
@@ -20,4 +20,6 @@ fn main() {
 
     println!("Read memory address: [{},{}], retrieved content: {}",
              memory_address[0], memory_address[1], &retrieved_data);
+
+    let _dev = eeprom.destroy(); // Get the I2C device back
 }
