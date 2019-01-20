9170f5ad369a15cc684dbf86acb3339cf71763c7
Diego Barrios Romero <eldruin@gmail.com>
2019-01-20 12:34:21 +0100

    Add tests protecting against writing to position over capacity

--- tests/interface.rs
+++ tests/interface.rs
@@ -377,6 +377,20 @@ macro_rules! cannot_write_invalid_addr_v2 {
 for_all_ics_with_2b_addr!(cannot_write_invalid_addr_v2);
 
 #[test]
+fn cannot_write_to_position_over_capacity_1byte() {
+    let mut eeprom = new_24x01(&[]);
+    assert_invalid_address(eeprom.write_byte(0xFF, 0xAB));
+    destroy(eeprom);
+}
+
+#[test]
+fn cannot_write_to_position_over_capacity_2bytes() {
+    let mut eeprom = new_24x256(&[]);
+    assert_invalid_address(eeprom.write_byte(0xFFFF, 0xAB));
+    destroy(eeprom);
+}
+
+#[test]
 fn can_use_device_address_for_memory_addressing_1byte() {
     let trans = [I2cTrans::write(DEV_ADDR | 0x7, vec![0xBC, 0xAB])];
     let mut eeprom = new_24x16(&trans);
@@ -385,7 +399,7 @@ fn can_use_device_address_for_memory_addressing_1byte() {
 }
 
 #[test]
-fn can_use_device_address_for_memory_addressing_2byte() {
+fn can_use_device_address_for_memory_addressing_2bytes() {
     let trans = [I2cTrans::write(DEV_ADDR | 0x3, vec![0xBC, 0xDE, 0xAB])];
     let mut eeprom = new_24xm02(&trans);
     eeprom.write_byte(0x3BCDE, 0xAB).unwrap();
