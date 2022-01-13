bc5ac96dc2c7427877e04b37fc9f62057ff14d03
Tobias Naumann <tobias.naumann@iml.fraunhofer.de>
2022-01-13 01:05:46 +0100

    Change the bitmask calculations for the devaddr method
    This also adds more documentation to the method which aids in
    understanding the concept, especially for users of devices which do not
    move memory address bits into the device address.

--- src/slave_addr.rs
+++ src/slave_addr.rs
@@ -18,11 +18,22 @@ impl SlaveAddr {
         }
     }
 
-    /// Get the device address possibly including some bits from the memory address
-    pub(crate) fn devaddr(self, memory_address: u32, address_bits: u8, shift: u8) -> u8 {
-        let devmask = ((1 << address_bits) - 1) >> shift;
-        let hi = (memory_address & !(1 << address_bits)) >> shift;
-        (self.addr() & !(devmask as u8)) | hi as u8
+    /// Get the device address possibly including some bits from the memory address, e.g. for
+    /// AT24C16 the 8 bit device address is: 1 0 1 0 A10 A9 A8 R/W , i.e. the highest 3 bits of
+    /// the memory address are moved into the device address.
+    ///
+    /// num_address_bits is the total number of address bits, shift is the number of address bits
+    /// which are transmitted separately. Theoretically, max(0, num_address_bits-shift) is the
+    /// number of address bits that are moved into the device address, but this overflows in many
+    /// cases and requires special handling for num_address_bits < shift.
+    pub(crate) fn devaddr(self, memory_address: u32, num_address_bits: u8, shift: u8) -> u8 {
+        // the part in parentheses creates num_address_bits ones; after right-shifting, 0..3 ones
+        // remain; the calculations have to be done in u32 to prevent overflow
+        let memmask: u32 = ((1 << num_address_bits) - 1) >> shift;
+        // the inverse is the part of the device address that we keep
+        let devmask = !memmask as u8;
+        let hi_addr_bits = memory_address >> shift;
+        (self.addr() & devmask) | hi_addr_bits as u8
     }
 }
 
@@ -54,5 +65,31 @@ mod tests {
             SlaveAddr::Alternative(true, false, false).addr()
         );
         assert_eq!(0b101_0111, SlaveAddr::Alternative(true, true, true).addr());
+    }
+
+    #[test]
+    fn assemble_devaddr() {
+        assert_eq!(0b101_0001, SlaveAddr::Default.devaddr(0b1_1111_1111, 9, 8));
+        assert_eq!(0b101_0000, SlaveAddr::Default.devaddr(0b0_1111_1111, 9, 8));
+        assert_eq!(
+            0b101_0011,
+            SlaveAddr::Default.devaddr(0b11_1111_1111, 10, 8)
+        );
+        assert_eq!(
+            0b101_0010,
+            SlaveAddr::Default.devaddr(0b10_1111_1111, 10, 8)
+        );
+        assert_eq!(
+            0b101_0111,
+            SlaveAddr::Default.devaddr(0b111_1111_1111, 11, 8)
+        );
+        assert_eq!(
+            0b101_0101,
+            SlaveAddr::Default.devaddr(0b101_1111_1111, 11, 8)
+        );
+        assert_eq!(
+            0b101_0101,
+            SlaveAddr::Default.devaddr(0b101_1111_1111_1111_1111, 19, 16)
+        );
     }
 }
